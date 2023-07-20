dcd9933f9379b9f67b335b95ed07c8b5020f5cd6
MahmoudHadmy <hamdy.xd55@gmail.com>
2023-07-21 01:55:07 +0300

    bubble sort

---
+++ print_array.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(const int *array, size_t size)
+{
+    size_t i;
+
+    i = 0;
+    while (array && i < size)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", array[i]);
+        ++i;
+    }
+    printf("\n");
+}
+#include "sort.h"
+/**
+ * swap - swap 2 elements in the array
+ * @array: the array
+ * @a: first index
+ * @b: second index
+*/
+void swap(int *array, size_t a, size_t b)
+{
+	int temp = array[a];
+	array[a] = array[b];
+	array[b] = temp;
+}
