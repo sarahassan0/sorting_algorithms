b8e02963b8d73842b56d92c568465729a2cf5785
sarahassan0 <sarraree5@gmail.com>
2023-07-21 03:55:46 +0300

    enhance task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -16,12 +16,13 @@ void selection_sort(int *array, size_t size)
         for (j = i + 1; j < size; j++)
         {
             if (array[min] > array[j])
-            {
                 min = j;
-            }
         }
-        swap(&array[min], &array[i]);
-        print_array(array, size);
+        if (min != i)
+        {
+            swap(&array[min], &array[i]);
+            print_array(array, size);
+        }
     }
 }
 
