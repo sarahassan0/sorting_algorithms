ab1a434b674eedf7f365063397f4f38d6e327b33
sarahassan0 <sarraree5@gmail.com>
2023-07-23 22:34:26 +0300

    fix betty

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -23,7 +23,7 @@ void shell_sort(int *array, size_t size)
 			{
 				if (array[j] != array[j - gap])
 					swap(&array[j], &array[j - gap]);
-				j - = gap;
+				j -= gap;
 			}
 		}
 		gap = (gap - 1) / 3;
