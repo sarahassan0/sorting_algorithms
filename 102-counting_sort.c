a23a006978b20caf4973cee7d7ff0a8ca16374ea
MahmoudHadmy <hamdy.xd55@gmail.com>
2023-07-25 02:20:47 +0300

    counting sort

---
+++ 102-counting_sort.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+size_t max(int *array, size_t size);
+/**
+ * counting_sort - counting sort algo
+ * @array: array that will be sorted
+ * @size: sizethe array
+ */
+void counting_sort(int *array, size_t size)
+{
+	size_t i, freq_len = max(array, size);
+	int *sorted = malloc(sizeof(int) * size);
+	int *freq = malloc((freq_len + 1) * sizeof(int));
+
+	if (!sorted || !array)
+		return;
+	for (i = 0; i <= freq_len; i++)
+		freq[i] = 0;
+	for (i = 0; i < size; i++)
+		freq[array[i]] += 1;
+	for (i = 1; i <= freq_len; i++)
+		freq[i] += freq[i - 1];
+	print_array(freq, freq_len + 1);
+	for (i = size - 1; (int)i >= 0; i--)
+		sorted[--freq[array[i]]] = array[i];
+	for (i = 0; i< size; i++)
+		array[i] = sorted[i];
+	free(sorted);
+	free(freq);
+}
+/**
+ * max - get the max elementthe array
+ * @array: array to get the max
+ * @size: size of the array
+ * Retuen: return max of the array
+ */
+size_t max(int *array, size_t size)
+{
+	size_t i;
+	int max = array[0];
+
+	for (i = 0; i < size; i++)
+		if (array[i] > max)
+			max = array[i];
+	return max;
+}
--- sort.h
+++ sort.h
@@ -30,5 +30,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void shell_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
 
 #endif
