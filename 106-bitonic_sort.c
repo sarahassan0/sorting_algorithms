311472ea82dd07437f3a3d0aa58090d511da6153
MahmoudHadmy <hamdy.xd55@gmail.com>
2023-07-26 03:30:05 +0300

    106-bitonic_sort.c

---
+++ 106-bitonic_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+/**
+ * bitonic_sort - bitonic sort algo
+ * @size: sizethe array
+ */
+void bitonic_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+	print_array(array, size);
+	printf("how u doing guys hope u r good");
+}
